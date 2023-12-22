a15fb71b0118bb17298845613746c8281d169f52
chen08209 <chen08209@gmail.com>
2023-12-22 09:12:42 +0800

    init FlClash

--- component/process/process.go
+++ component/process/process.go
@@ -22,12 +22,8 @@ func FindProcessName(network string, srcIP netip.Addr, srcPort int) (uint32, str
 	return findProcessName(network, srcIP, srcPort)
 }
 
-// PackageNameResolver
-// never change type traits because it's used in CMFA
 type PackageNameResolver func(metadata *C.Metadata) (string, error)
 
-// DefaultPackageNameResolver
-// never change type traits because it's used in CMFA
 var DefaultPackageNameResolver PackageNameResolver
 
 func FindPackageName(metadata *C.Metadata) (string, error) {
